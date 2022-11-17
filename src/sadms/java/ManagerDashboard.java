e712c79c4383c077924bfe05031bb4594d413a28
Durvesh <durveshmore2003@gmail.com>
2022-11-17 23:59:54 +0530

    /* Made Parital Navbar */

--- src/sadms/java/ManagerDashboard.java
+++ src/sadms/java/ManagerDashboard.java
@@ -1,5 +1,14 @@
 package sadms.java;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import sadms.Main;
+
 public class ManagerDashboard {
-    
+
+    @FXML void Log_Out(ActionEvent event) throws Exception
+    {
+        Main.setRoot("ManagerLogin");
+    }
+
 }
