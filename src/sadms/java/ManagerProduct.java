9806ceeb417881bcf7f55d11c3d39a7a6e4aade1
Durvesh <durveshmore2003@gmail.com>
2022-11-19 14:05:54 +0530

    /* Created backend for Manager Product */

--- src/sadms/java/ManagerProduct.java
+++ src/sadms/java/ManagerProduct.java
@@ -1,13 +1,36 @@
 package sadms.java;
 
+import org.bson.Document;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import sadms.Main;
 
 public class ManagerProduct {
-    @FXML Label manager;
+    @FXML GridPane Details;
+    @FXML Label manager, ErrorMessage;
+    @FXML TextField ProductId, ProductName, SellingPrice, CostPrice, Description;
+    @FXML Button AddProduct, UpdateProduct, DeleteProduct;
+    Document value;
 
+    public ManagerProduct()
+    {
+        MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017");
+        MongoClient client = new MongoClient(uri);
+        MongoDatabase database = client.getDatabase("sadms");
+        MongoCollection<Document> collection = database.getCollection("Product");
+        Document query = new Document("ProductId","Nex123");
+        value = collection.find(query).first();
+        client.close();
+    }
     public void initialize()
     {
         manager.setText(ManagerLogin.value.getString("ManagerName"));
@@ -35,5 +58,27 @@ public class ManagerProduct {
     @FXML void ChartGraphClicked(ActionEvent event) throws Exception
     {
         Main.setRoot("ManagerChartGraph");
+    }
+    @FXML void ProductIdEntered(ActionEvent event) throws Exception
+    {
+        if(value.getString("ProductId").equals(ProductId.getText()))
+        {
+            Details.setVisible(true);
+            ErrorMessage.setVisible(false);
+            ProductName.setText(value.getString("ProductName"));
+            SellingPrice.setText(value.getInteger("SellingPrice").toString());
+            CostPrice.setText(value.getInteger("CostPrice").toString());
+            AddProduct.setVisible(false);
+            UpdateProduct.setVisible(true);
+            DeleteProduct.setVisible(true);
+        }
+        else
+        {
+            Details.setVisible(false);
+            ErrorMessage.setVisible(true);
+            AddProduct.setVisible(true);
+            UpdateProduct.setVisible(false);
+            DeleteProduct.setVisible(false);
+        }
     }
 }
