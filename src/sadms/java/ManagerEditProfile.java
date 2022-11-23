0d34d483bc2faac87286a662196548c711900472
Durvesh <durveshmore2003@gmail.com>
2022-11-23 22:46:22 +0530

    Manager Edit Profile Completed

--- src/sadms/java/ManagerEditProfile.java
+++ src/sadms/java/ManagerEditProfile.java
@@ -1,14 +1,24 @@
 package sadms.java;
+import org.bson.Document;
+
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import sadms.Main;
 
 public class ManagerEditProfile {
-    @FXML Label manager;
+    @FXML Label manager, ErrorMessage;
+    @FXML TextField ManagerName;
+    @FXML PasswordField EnterPassword, ConfirmPassword;
 
     public void initialize()
     {
         manager.setText(ManagerLogin.value.getString("ManagerName"));
+        ErrorMessage.setText("");
     }
     @FXML void Log_Out() throws Exception
     {
@@ -45,5 +55,41 @@ public class ManagerEditProfile {
     @FXML void ChartGraphClicked() throws Exception
     {
         Main.setRoot("ManagerChartGraph");
+    }
+
+    @FXML void UpdateClicked() throws Exception
+    {
+        ErrorMessage.setText("");
+        if(ManagerName.getText().equals("") || EnterPassword.getText().equals("") || ConfirmPassword.getText().equals(""))
+        {
+            ErrorMessage.setText("** Fill all Text-Fields **");
+        }
+        else if(EnterPassword.getText().equals(ConfirmPassword.getText()))
+        {
+            Main.openCon("managerlogin");
+            Document query = new Document("ManagerName",ManagerName.getText());
+            Document value = Main.collection.find(query).first();
+            if(value == null || value.getString("ManagerName").equals(ManagerName.getText()))
+            {
+                Main.collection.updateOne(Filters.eq("ManagerName",manager.getText()), Updates.set("ManagerName",ManagerName.getText()));
+                Main.collection.updateOne(Filters.eq("ManagerName",manager.getText()),Updates.set("Password",EnterPassword.getText()));
+                ErrorMessage.setText("** Credentials Updated Successfully **");
+            }
+            else
+            {
+                ErrorMessage.setText("** Manager with this Id already exists **");
+            }
+            Main.closeCon();
+        }
+        else
+        {
+            ErrorMessage.setText("** Password doesn't match **");
+        }
+    }
+    @FXML void ClearClicked() throws Exception
+    {
+        ManagerName.clear();
+        EnterPassword.clear();
+        ConfirmPassword.clear();
     }
 }
