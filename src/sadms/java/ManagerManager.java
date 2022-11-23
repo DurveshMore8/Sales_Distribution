6a89ff4a4dcda4d605ca2f1f12f65bfb02897e26
Durvesh <durveshmore2003@gmail.com>
2022-11-23 15:40:36 +0530

    Manager Add Completed

--- src/sadms/java/ManagerManager.java
+++ src/sadms/java/ManagerManager.java
@@ -1,14 +1,31 @@
 package sadms.java;
+import org.bson.Document;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import sadms.Main;
 
 public class ManagerManager {
-    @FXML Label manager;
+    @FXML Label manager, ErrorMessage;
+    @FXML TextField ManagerId, Name, Gender, DateofBirth, Age, Phone, EmailId, ManagerName;
+    @FXML GridPane Details;
+    @FXML Button AddManager, UpdateManager, DeleteManager;
+    static String sendManagerId, sendManagerName;
+    public static String sendmessage;
 
     public void initialize()
     {
         manager.setText(ManagerLogin.value.getString("ManagerName"));
+        Details.setVisible(false);
+        ErrorMessage.setVisible(false);
+        if(sendmessage != "")
+        {
+            ErrorMessage.setText(sendmessage);
+            ErrorMessage.setVisible(true);
+            sendmessage = "";
+        }
     }
     @FXML void Log_Out() throws Exception
     {
@@ -45,5 +62,57 @@ public class ManagerManager {
     @FXML void ChartGraphClicked() throws Exception
     {
         Main.setRoot("ManagerChartGraph");
+    }
+
+    @FXML void ManagerIdEntered() throws Exception
+    {
+        Main.openCon("managerdata");
+        Document query = new Document("ManagerId", ManagerId.getText());
+        Document value = Main.collection.find(query).first();
+        if(value == null || value.getBoolean("SuperManager") != null)
+        {
+            Details.setVisible(false);
+            ErrorMessage.setText("** No Manager Available **");
+            ErrorMessage.setVisible(true);
+            UpdateManager.setDisable(true);
+            DeleteManager.setDisable(true);   
+        }
+        else if(value.getString("ManagerName").equals(manager.getText()))
+        {
+            Details.setVisible(false);
+            ErrorMessage.setText("** Go to Edit Profile **");
+            ErrorMessage.setVisible(true);
+            UpdateManager.setDisable(true);
+            DeleteManager.setDisable(true);   
+        }
+        else
+        {
+            Details.setVisible(true);
+            ErrorMessage.setVisible(false);
+            Name.setText(value.getString("Name"));
+            Gender.setText(value.getString("Gender"));
+            DateofBirth.setText(value.getString("DateofBirth"));
+            Age.setText(value.getInteger("Age").toString());
+            Phone.setText(value.getString("Phone"));
+            EmailId.setText(value.getString("EmailId"));
+            ManagerName.setText(value.getString("ManagerName"));
+            UpdateManager.setDisable(false);
+            DeleteManager.setDisable(false);
+            sendManagerId = ManagerId.getText();
+            sendManagerName = ManagerName.getText();
+        }
+        Main.closeCon();
+    }
+    @FXML void AddManagerClicked() throws Exception
+    {
+        Main.setRoot("ManagerManagerAdd");
+    }
+    @FXML void UpdateManagerClicked() throws Exception
+    {
+        Main.setRoot("ManagerManagerUpdate");
+    }
+    @FXML void DeleteManagerClicked() throws Exception
+    {
+
     }
 }
--- src/sadms/java/ManagerManagerAdd.java
+++ src/sadms/java/ManagerManagerAdd.java
@@ -9,9 +9,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import sadms.Main;
 
-public class ManagerEmployeeAdd {
+public class ManagerManagerAdd {
     @FXML Label manager,ErrorMessage;
-    @FXML TextField EmployeeId, Name, EmployeeName, DateofBirth, Phone, GmailId;
+    @FXML TextField ManagerId, Name, DateofBirth, Phone, EmailId;
     @FXML ToggleGroup Gender;
 
     public void initialize()
@@ -62,54 +62,49 @@ public class ManagerEmployeeAdd {
     @FXML void AddClicked() throws Exception
     {
         ErrorMessage.setVisible(false);
-        if(EmployeeId.getText().equals("") || Name.getText().equals("") || EmployeeName.getText().equals("") || DateofBirth.getText().equals("") || Phone.getText().equals("") || GmailId.getText().equals(""))
+        if(ManagerId.getText().equals("") || Name.getText().equals("") || (RadioButton) Gender.getSelectedToggle() == null || DateofBirth.getText().equals("") || Phone.getText().equals("") || EmailId.getText().equals(""))
         {
             ErrorMessage.setText("** Fill all Text-Fields **");
             ErrorMessage.setVisible(true);
             return;
         }
-        Main.openCon("employeedata");
-        Document query = new Document("EmployeeId",EmployeeId.getText());
+        Main.openCon("managerdata");
+        Document query = new Document("ManagerId",ManagerId.getText());
         Document value = Main.collection.find(query).first();
         if(value == null)
         {
             try
             {
-                
-                query.append("Name",Name.getText()).append("EmployeeName", EmployeeName.getText()).append("Gender", ((RadioButton) Gender.getSelectedToggle()).getText()).append("DateofBirth",DateofBirth.getText()).append("Age", Period.between(LocalDate.parse(DateofBirth.getText()), LocalDate.now()).getYears()).append("Phone",Phone.getText()).append("GmailId",GmailId.getText()).append("ManagerName",manager.getText());
+                query.append("Name",Name.getText()).append("Gender", ((RadioButton) Gender.getSelectedToggle()).getText()).append("DateofBirth",DateofBirth.getText()).append("Age", Period.between(LocalDate.parse(DateofBirth.getText()), LocalDate.now()).getYears()).append("Phone",Phone.getText()).append("EmailId",EmailId.getText()).append("ManagerName",Name.getText().replaceAll(" ", "")).append("AddedBy",manager.getText());
                 Main.collection.insertOne(query);
-                ManagerEmployee.sendmessage = "** Employee Added Successfully **";
+                ManagerManager.sendmessage = "** Manager Added Successfully **";
                 Main.closeCon();
-                Main.openCon("employeelogin");
-                query = new Document("EmployeeName",EmployeeName.getText()).append("Password",EmployeeName.getText());
+                Main.openCon("managerlogin");
+                query = new Document("ManagerName", Name.getText().replaceAll(" ", "")).append("EmailId", EmailId.getText()).append("Password", Name.getText().replaceAll(" ", ""));
                 Main.collection.insertOne(query);
-                Main.setRoot("ManagerEmployee");
+                Main.setRoot("ManagerManager");
+                Main.closeCon();
             }
             catch(Exception e)
             {
-                ErrorMessage.setText("** Enter Date in Proper Format **");
+                ErrorMessage.setText("** Fill all Text-Fields Properly **");
                 ErrorMessage.setVisible(true);
-                e.printStackTrace();
-            }
-            finally
-            {
-                Main.closeCon();
             }
         }
         else
         {
-            ErrorMessage.setText("** Employee of this Id already exists **");
+            ErrorMessage.setText("** Manager of this Id already exists **");
             ErrorMessage.setVisible(true);
         }
     }
+
     @FXML void ClearClicked() throws Exception
     {
-        EmployeeId.clear();
+        ManagerId.clear();
         Name.clear();
-        EmployeeName.clear();
         ((RadioButton) Gender.getSelectedToggle()).setSelected(false);
         DateofBirth.clear();
         Phone.clear();
-        GmailId.clear();
+        EmailId.clear();
     }
 }
